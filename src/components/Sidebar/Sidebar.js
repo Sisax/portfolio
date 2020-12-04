e88eb4b1eaf7170f5ff7789d3d417fe671395881
Sisa <tamas.sisa@gmail.com>
2020-12-04 22:56:02 +0100

    initial commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <aside>
+      <ul>
+        <li>Introduction</li>
+        <li>Skills</li>
+        <li>Education and Experience</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar;
